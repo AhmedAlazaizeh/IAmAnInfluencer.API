77060c67575d70e3edc51bc64690af9fab7993f6
AhmedAlazaizeh <azaizeh44@gmail.com>
2021-11-12 15:41:26 +0200

    Add project files.

---
+++ IAmAnInfluencer.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IAmAnInfluencer.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ IAmAnInfluencer.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IAmAnInfluencer.Infra
+{
+    public class Class1
+    {
+    }
+}
